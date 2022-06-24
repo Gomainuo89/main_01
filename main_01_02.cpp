d3f8f9700653a8a9b01cb8c00d8b42466e94b25d
Gomainuo89 <108019573+Gomainuo89@users.noreply.github.com>
2022-06-24 11:47:54 +0100

    Create main_01_02.cpp

---
+++ main_01_02.cpp
@@ -0,0 +1,14 @@
+
+/* 
+     While Loop. 
+                   */
+#include <iostream>
+using namespace std ;
+int main(){
+	int x = 0 ; 
+	while (x < 100){
+		cout<<x <<endl;
+		x+=1;
+	}
+	return 0 ;
+}
